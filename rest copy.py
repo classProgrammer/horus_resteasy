cad0424a555648ff594b24a3dde74a84485032d6
classProgrammer <gerald.application@gmail.com>
2020-06-10 12:18:27 +0200

    update: service

---
+++ rest copy.py
@@ -0,0 +1,214 @@
+from flask import Flask, request, make_response, jsonify, abort
+import sys
+from datetime import date
+import re
+import mongodbwrapper as dbwrapper
+
+restService = Flask(__name__)
+
+# ----------------------------------------------
+# -------------------- DATA --------------------
+# ----------------------------------------------
+dialogflow_request = [""]
+watson_request = [""]
+
+# ----------------------------------------------
+# --------------- UTILITY METHODS --------------
+# ----------------------------------------------
+def now():
+    return date.today().strftime("%d.%m.%Y")
+
+def asJsonResponse(data):
+    return make_response(jsonify(data))
+
+def extractDob(date):
+    pattern = re.compile(r"[-T+]")
+    return '.'.join(reversed(pattern.split(date)[0:3]))
+
+# ----------------------------------------------
+# ----------------- STARTUP --------------------
+# ----------------------------------------------
+@restService.route("/")
+def default():
+    return "The REST service is up"
+
+# ----------------------------------------------
+# ------------------- RASA ---------------------
+# ----------------------------------------------
+# entry point for Rasa call
+@restService.route("/sick", methods=['POST'])
+def postSick():
+    data  = request.get_json()
+    name  = data['name'].lower()
+    today = now()
+
+    user = dbwrapper.addSicknessToUser(name, today)
+
+    if user is None:
+        return "ERROR: invalid parameters", 400
+
+    return {
+        "name": user['name'],
+        "group": user["group"],
+        "dob": user["dob"],
+        "sick_on": today
+    }, 200
+
+# ----------------------------------------------
+# ---------------- DIALOGFLOW ------------------
+# ----------------------------------------------
+def dialogflowHandler():
+    req = request.get_json(force=True)
+    dialogflow_request[0] = req #DEV
+    intent = req.get('queryResult').get('intent').get("displayName")
+
+    if (intent == "Sickness"):
+        params = req.get('queryResult').get('parameters')
+        name = params.get("name").get("name").lower()
+        today = now()
+
+        user = dbwrapper.addSicknessToUser(name, today)
+
+        if user is None:
+            return {'fulfillmentText': 'Die eingegebenen Daten sind ungültig. Die Konversation wird zurückgesetzt'}
+        
+        return {}
+
+    if (intent == "Urlaub nehmen"):
+        params = req.get('queryResult').get('parameters')
+        name = params.get('name').get('name').lower()
+        start = params.get('start')
+        end = params.get('end')
+
+        bookingWorked = dbwrapper.addVacation(name, start, end)
+
+        if not bookingWorked:
+            return {'fulfillmentText': 'Die eingegebenen Daten sind ungültig. Die Konversation wird zurückgesetzt'}
+
+        return {}
+
+    return {'fulfillmentText': f'Intent {intent} unbekannt.'}
+
+# dialogflow request entry point
+@restService.route('/dialogflow/webhook', methods=['GET', 'POST'])
+def dialogflowWebhook():
+    return asJsonResponse(dialogflowHandler())
+
+# ----------------------------------------------
+# ---------------- DIALOGFLOW ------------------
+# ----------------------------------------------
+def personNotFound(str):
+    return "Person not found. Enter " + str + " to retry."
+
+def dialogflowRequestHandler():
+    req = request.get_json(force=True)
+    dialogflow_request[0] = req #DEV
+    intent = req.get('queryResult').get('intent').get("displayName")
+
+    if (intent == "Sickness"):
+        params = req.get('queryResult').get('parameters')
+        name = params.get("name").get("name").lower()
+        today = now()
+
+        user = None
+
+        if user is None:
+            return {'fulfillmentText': personNotFound('sick')}
+        
+        return {}
+
+    if (intent == "Vacation"):
+        params = req.get('queryResult').get('parameters')
+        name = params.get('name').get('name').lower()
+        start = params.get('start')
+        end = params.get('end')
+
+        bookingWorked = dbwrapper.addVacation(name, start, end)
+
+        if not bookingWorked:
+            return {'fulfillmentText': personNotFound('vacation')}
+
+        return {}
+
+    return {'fulfillmentText': f'Intent {intent} unknown.'}
+
+# dialogflow request entry point
+@restService.route('rest/dialogflow/webhook', methods=['GET', 'POST'])
+def dialogflowRequestEntryPoint():
+    return asJsonResponse(dialogflowRequestHandler())
+
+# ----------------------------------------------
+# ------------------ WATSON --------------------
+# ----------------------------------------------  
+def watsonHandler():
+    req = request.get_json(force=True)
+    intent = req.get('intent')
+    watson_request[0] = req #DEV
+    if (intent == "Sickness"):
+
+        name = req.get("person_name").lower()
+        today = now()
+        
+        user = dbwrapper.addSicknessToUser(name, today)
+
+        if user is None:
+            return {'error': 'Die eingegebenen Daten sind ungültig.'}
+        
+        return {'OK': f'Gute Besserung {req.get("person_name")}!'}
+
+    return {'error': f'Intent {intent} unbekannt.'}
+
+# watson request entry point
+@restService.route('/watson/webhook', methods=['POST'])
+def watsonWebhook():
+    return asJsonResponse(watsonHandler())
+
+# ----------------------------------------------
+# -------------- DEVELOPMENT -------------------
+# ----------------------------------------------
+@restService.route("/sick/all", methods=['GET'])
+def getSickAll():
+    return asJsonResponse(dbwrapper.getAllSickUsers()), 200
+
+# For dev purpose/check
+@restService.route("/sick", methods=['GET'])
+def getSick():
+    data = request.get_json()
+    name = data['name'].lower()
+
+    user = dbwrapper.findByName(name)
+
+    if user is None or user.count() == 0:
+        return "ERROR: invalid parameters", 400
+
+    sicknesses = dbwrapper.findSicknessByUserId(user.next()['_id'])
+
+    if sicknesses.count() == 0:
+        return "No entry found", 200
+    else:
+        return asJsonResponse(sicknesses), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+# for dev purpose, to check what the framewokrs send
+@restService.route("/dialogflow/requests", methods=['GET'])
+def getRequests():
+    return asJsonResponse(dialogflow_request), 200
+
+@restService.route("/watson/requests", methods=['GET'])
+def getWatsonRequests():
+    return asJsonResponse(watson_request), 200
+
+date_time_format = '%Y-%m-%dT%H:%M:%S%z'
+
+@restService.route("/vacation/all", methods=['GET'])
+def getVacationAll():
+    vacations = []
+
+    for vacation in dbwrapper.getAllVacations():
+        vacations.append({
+            'user': vacation['user']['name'],
+            'start': vacation['start'],
+            'end': vacation['end']
+        })
+
+
+    return asJsonResponse(vacations), 200
--- rest.py
+++ rest.py
@@ -2,8 +2,6 @@ from flask import Flask, request, make_response, jsonify, abort
 import sys
 from datetime import date
 import re
-from bson import json_util
-import mongodbwrapper as dbwrapper
 
 restService = Flask(__name__)
 
@@ -34,117 +32,91 @@ def default():
     return "The REST service is up"
 
 # ----------------------------------------------
-# ------------------- RASA ---------------------
-# ----------------------------------------------
-# entry point for Rasa call
-@restService.route("/sick", methods=['POST'])
-def postSick():
-    data  = request.get_json()
-    name  = data['name'].lower()
-    today = now()
-
-    user = dbwrapper.addSicknessToUser(name, today)
-
-    if user is None:
-        return "ERROR: invalid parameters", 400
-
-    return {
-        "name": user['name'],
-        "group": user["group"],
-        "dob": user["dob"],
-        "sick_on": today
-    }, 200
-
-# ----------------------------------------------
 # ---------------- DIALOGFLOW ------------------
 # ----------------------------------------------
-def dialogflowHandler():
+def personNotFound(str):
+    return "Person not found. Enter " + str + " to retry."
+
+def dialogflowRequestHandler():
     req = request.get_json(force=True)
     dialogflow_request[0] = req #DEV
-    intent = req.get('queryResult').get('intent').get("displayName")
+    intent = req.get('queryResult').get('intent').get('displayName')
 
-    if (intent == "Sickness"):
-        params = req.get('queryResult').get('parameters')
-        name = params.get("name").get("name").lower()
-        today = now()
+    if (intent == 'Sickness'):
+        params = req.get('queryResult').get('outputContexts')[0].get('parameters')
+        employee_name = params.get('employee').get('name').lower()
+    elif (intent == 'Sickness Confirmed'):
+        params = req.get('queryResult').get('outputContexts')[0].get('parameters')
+        employee_name = params.get('employee').get('name').lower()
+        # today = now()
 
-        user = dbwrapper.addSicknessToUser(name, today)
-
-        if user is None:
-            return {'fulfillmentText': 'Die eingegebenen Daten sind ungültig. Die Konversation wird zurückgesetzt'}
+        return {
+            "fulfillmentMessages": [
+                {
+                "text": {
+                    "text": [
+                    "Text response from webhook"
+                    ]
+                }
+                }
+            ]
+        }
         
-        return {}
+    elif (intent == 'Vacation'):
+        # params = req.get('queryResult').get('parameters')
+        # name = params.get('name').get('name').lower()
+        # start = params.get('start')
+        # end = params.get('end')
 
-    if (intent == "Urlaub nehmen"):
-        params = req.get('queryResult').get('parameters')
-        name = params.get('name').get('name').lower()
-        start = params.get('start')
-        end = params.get('end')
+        return {'fulfillmentText': 'vacation intent'}
 
-        bookingWorked = dbwrapper.addVacation(name, start, end)
+    return {
+            "fulfillmentMessages": [
+                {
+                "text": {
+                    "text": [
+                    f'Intent {intent} unknown.'
+                    ]
+                }
+                }
+            ]
+        }
 
-        if not bookingWorked:
-            return {'fulfillmentText': 'Die eingegebenen Daten sind ungültig. Die Konversation wird zurückgesetzt'}
-
-        return {}
-
-    return {'fulfillmentText': f'Intent {intent} unbekannt.'}
-
-# dialogflow request entry point
-@restService.route('/dialogflow/webhook', methods=['GET', 'POST'])
-def dialogflowWebhook():
-    return asJsonResponse(dialogflowHandler())
-
-# ----------------------------------------------
-# ------------------ WATSON --------------------
-# ----------------------------------------------  
-def watsonHandler():
+def watsonRequestHandler():
     req = request.get_json(force=True)
     intent = req.get('intent')
     watson_request[0] = req #DEV
-    if (intent == "Sickness"):
+    if (intent == "Sick"):
 
         name = req.get("person_name").lower()
         today = now()
-        
-        user = dbwrapper.addSicknessToUser(name, today)
 
-        if user is None:
-            return {'error': 'Die eingegebenen Daten sind ungültig.'}
-        
-        return {'OK': f'Gute Besserung {req.get("person_name")}!'}
+    return {
+        'OK': True,
+        'Department': 'Marketing'
+        }
 
-    return {'error': f'Intent {intent} unbekannt.'}
+# dialogflow request entry point
+@restService.route('/dialogflow/webhook', methods=['GET', 'POST'])
+def dialogflowRequestEntryPoint():
+    return asJsonResponse(dialogflowRequestHandler()), 200
 
-# watson request entry point
-@restService.route('/watson/webhook', methods=['POST'])
-def watsonWebhook():
-    return asJsonResponse(watsonHandler())
+@restService.route('/watson/webhook', methods=['GET', 'POST'])
+def watsonRequestEntryPoint():
+    return asJsonResponse(watsonRequestHandler()), 200
+
 
 # ----------------------------------------------
 # -------------- DEVELOPMENT -------------------
 # ----------------------------------------------
 @restService.route("/sick/all", methods=['GET'])
 def getSickAll():
-    return asJsonResponse(dbwrapper.getAllSickUsers()), 200
+    return {}, 200
 
 # For dev purpose/check
 @restService.route("/sick", methods=['GET'])
 def getSick():
-    data = request.get_json()
-    name = data['name'].lower()
-
-    user = dbwrapper.findByName(name)
-
-    if user is None or user.count() == 0:
-        return "ERROR: invalid parameters", 400
-
-    sicknesses = dbwrapper.findSicknessByUserId(user.next()['_id'])
-
-    if sicknesses.count() == 0:
-        return "No entry found", 200
-    else:
-        return json_util.dumps(sicknesses), 200, {'Content-Type': 'application/json; charset=utf-8'}
+    return {}, 200
 
 # for dev purpose, to check what the framewokrs send
 @restService.route("/dialogflow/requests", methods=['GET'])
@@ -159,14 +131,4 @@ date_time_format = '%Y-%m-%dT%H:%M:%S%z'
 
 @restService.route("/vacation/all", methods=['GET'])
 def getVacationAll():
-    vacations = []
-
-    for vacation in dbwrapper.getAllVacations():
-        vacations.append({
-            'user': vacation['user']['name'],
-            'start': vacation['start'],
-            'end': vacation['end']
-        })
-
-
-    return asJsonResponse(vacations), 200
+    return {}, 200
